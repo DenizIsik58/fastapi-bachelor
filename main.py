68247ca7b8c057d85a33c426034f2c0b0ae06da0
Deni <deni@itu.dk>
2023-03-23 21:50:00 +0100

    Add an authentication endpoint to refresh tokens

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import os
-
 import dotenv
 import uvicorn
 from fastapi import FastAPI
-from routers import login, products, register
+from routers import login, products, register,authentication
 
 api = FastAPI()
 
 api.include_router(login.login_router)
 api.include_router(products.products_router, prefix="/products")
 api.include_router(register.register_router, prefix="/register")
+api.include_router(authentication.authentication_router, prefix="/")
 
 @api.get("/")
 async def root():
@@ -23,4 +22,3 @@ async def startup_event():
 
 if __name__ == "__main__":
     uvicorn.run("main:api", host="0.0.0.0", port=8000, log_level="info", reload=True)
-
---
+++ routers/authentication.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends
+from fastapi_jwt_auth import AuthJWT
+
+authentication_router = APIRouter()
+
+@authentication_router.post('/')
+def refresh(Authorize: AuthJWT = Depends()):
+    """
+    The jwt_refresh_token_required() function ensures a valid refresh
+    token is present in the request before running any code below that function.
+    we can use the get_jwt_subject() function to get the subject of the refresh
+    token, and use the create_access_token() function again to make a new access token
+    """
+    Authorize.jwt_refresh_token_required()
+
+    current_user = Authorize.get_jwt_subject()
+    new_access_token = Authorize.create_access_token(subject=current_user)
+    return {"access_token": new_access_token}
