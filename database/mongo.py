acc0c0c3c7c19ac4108370b42276a851a8076b87
Deni <deni@itu.dk>
2023-03-24 11:18:45 +0100

    Clean up in database and remove async functions

--- database/mongo.py
+++ database/mongo.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 import pymongo
 
+from util.json_manager import serialize_models
+
 load_dotenv()
 connection = os.getenv("CONNECTION_STRING")
 mongo_client = pymongo.MongoClient(connection)
@@ -14,8 +16,15 @@ def get_collection(collection):
     return db.get_collection(collection)
 
 
-async def get_all_items_from_collection(collection):
-    return list(collection.find({}))
+def get_all_items_from_collection(collection):
+    return list(serialize_models(collection.find({})))
+
+def get_all_reviews_by_product_name(product_name):
+    product = get_collection("products").find_one({"name": product_name})
+    if product is None:
+        return None
+    return list(serialize_models(product["reviews"]))
+    #return list(serialize_models(get_collection("products").find({"reviews": {"$exists": True}})))
 
 
 def insert_to_collection(collection, item):
@@ -23,8 +32,16 @@ def insert_to_collection(collection, item):
     # Users, products, etc
     return collection.insert_one(item)
 
+def get_single_item(collection, key, value):
+    return serialize_models(collection.find_one({key: value
+                                }))
 
-async def delete_from_collection(collection, item):
+## count_documents is not synchronous
+def get_count_items(collection, key, value):
+    return collection.count_documents({key: value})
+
+
+def delete_from_collection(collection, item):
     # Delete any item to a collection
     # Users, products, etc
     return collection.delete_one(item)
