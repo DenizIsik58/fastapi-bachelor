c5de44b08ebd28d9245f4cdd34bbca993c2989ee
Deni <deni@itu.dk>
2023-03-25 15:10:51 +0100

    Remove test secret

--- base_models/settings.py
+++ base_models/settings.py
@@ -1,8 +1,8 @@
 import os
 
-from dotenv import dotenv_values
+from dotenv import load_dotenv
 from pydantic import BaseModel
 
-
+load_dotenv()
 class Settings(BaseModel):
-    authjwt_secret_key: str = "ewkjn34lrgjeklrj23lk4jnr"
+    authjwt_secret_key: str = os.getenv("secret")
